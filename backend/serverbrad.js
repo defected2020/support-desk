c4310dcbd0e5f55b9527a856f801121332760f5d
George B <george@wizardofweb.com>
2022-07-13 18:50:13 +0800

    fixed server.js config

--- backend/serverbrad.js
+++ backend/serverbrad.js
@@ -4,9 +4,9 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 
-//connect to databse
+// Connect to database
 connectDB()
 
 const app = express()
@@ -20,9 +20,10 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-  // set build folder as static
+  // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
+  // FIX: below code fixes app crashing on refresh in deployment
   app.get('*', (_, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
   })
